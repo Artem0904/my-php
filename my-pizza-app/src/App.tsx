b494b59d48ebe6dc8c1fe2d5c61ae1ac769fa2af
DEV <dev@step.rivne>
2024-06-14 19:04:09 +0300

    First comit

---
+++ my-pizza-app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import LoginPage from "./components/login/LoginPage.tsx";
+
+function App() {
+
+    return (
+        <>
+            <LoginPage/>
+        </>
+    )
+}
+
+export default App
